0765293b4d6acddff9d398a02db5238639985bd8
minpeter <kali2005611@gmail.com>
2021-04-23 12:02:38 +0900

    auto_runner.py add and ver 1.2

--- main.py
+++ main.py
@@ -7,33 +7,34 @@ from getUserInfo import getUserInfo
 from registerServey import registerServey
 import json
 
+def selftest():
 
-with open('personalData.json', 'r') as f:
-    json_data = json.load(f)
+    with open('personalData.json', 'r') as f:
+        json_data = json.load(f)
 
-schoolName = json_data["schoolName"]
-studentName = json_data["studentName"]
-studentBirth = json_data["studentBirth"]
-loginType = json_data["loginType"]
-schoolRegion = json_data["schoolRegion"]
-schoolType = json_data["schoolType"]
+    schoolName = json_data["schoolName"]
+    studentName = json_data["studentName"]
+    studentBirth = json_data["studentBirth"]
+    loginType = json_data["loginType"]
+    schoolRegion = json_data["schoolRegion"]
+    schoolType = json_data["schoolType"]
 
-print(json.dumps(json_data))
+    print(json.dumps(json_data))
 
-orgCode = searchSchool.searchSchool(schoolName, loginType, schoolRegion, schoolType)
+    orgCode = searchSchool.searchSchool(schoolName, loginType, schoolRegion, schoolType)
 
-token = findUser.findUser(orgCode, loginType, studentName, studentBirth)
-print(token)
-
-if hasPassword.hasPassword(token) == False:
-    print("자가진단 페이지에서 초기 비밀번호를 설정해주세요")
-else:
-    token = validatePassword(token)
+    token = findUser.findUser(orgCode, loginType, studentName, studentBirth)
     print(token)
-    token = selectUserGroup(token)
-    print(token)
-    token = getUserInfo(token)
-    print(token)
-    print(registerServey(schoolName, token))
 
-    
+    if hasPassword.hasPassword(token) == False:
+        print("자가진단 페이지에서 초기 비밀번호를 설정해주세요")
+    else:
+        token = validatePassword(token)
+        print(token)
+        token = selectUserGroup(token)
+        print(token)
+        token = getUserInfo(token)
+        print(token)
+        print(registerServey(schoolName, token))
+
+        
